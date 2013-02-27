c16f206070b32bd768ec5a5650962a707e637793
XiongBiao <xiongbiao@oa.kkalk.cn>
2013-02-27 15:30:45 +0800

    add daima

---
+++ CountrysideRacing/src/com/bn/carracer/BNShape.java
@@ -0,0 +1,14 @@
+package com.bn.carracer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+abstract class BNShape {
+ 
+	float scale;//尺寸比例
+	public BNShape(float scale)
+	{
+		this.scale=scale;
+	}
+	public abstract void drawSelf(GL10 gl,int texId,int number);
+}
+    
---
+++ gohome/src/com/android/go/home/GohomeActivity.java
@@ -0,0 +1,23 @@
+package com.android.go.home;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GohomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }
+    
+    public void c(){
+    	
+    }
+    
+    /**
+     * view
+     */
+    
+}
